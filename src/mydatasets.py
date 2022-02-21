b04341978082e8003a97ca57609052e486584f98
Daniel Bear <dbear9887@gmail.com>
2022-02-21 14:01:37 -0800

    switch to png datareader

--- src/mydatasets.py
+++ src/mydatasets.py
@@ -1,11 +1,16 @@
 import glob
-
+import json
 import numpy as np
 from PIL import Image
 import torch
 import torchvision.transforms as transforms
 import dorsalventral.data.tdw as tdw
 import dorsalventral.data.utils as data_utils
+
+from pathlib import Path
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision.io import read_image
 
 ## RAFT
 import os, sys, copy
@@ -161,75 +166,143 @@ class Mydatasets(torch.utils.data.Dataset):
 
         return out_data, out_t, out_t_aff
 
-class TdwAffinityDataset(TdwFlowDataset):
+# class TdwAffinityDataset(TdwFlowDataset):
 
-    def __init__(self, aff_r, num_levels=5,
-                 raft_ckpt=os.path.join(RAFT_DIR, 'models', 'raft-sintel.pth'),
-                 raft_args={'test_mode': True, 'iters': 24},
-                 full_supervision=False,
-                 flow_thresh=0.5,
-                 *args, **kwargs):
+#     def __init__(self, aff_r, num_levels=5,
+#                  raft_ckpt=os.path.join(RAFT_DIR, 'models', 'raft-sintel.pth'),
+#                  raft_args={'test_mode': True, 'iters': 24},
+#                  full_supervision=False,
+#                  flow_thresh=0.5,
+#                  *args, **kwargs):
 
-        super(TdwAffinityDataset, self).__init__(
-            get_gt_segments=True,
-            *args, **kwargs)
-        self.aff_r = aff_r
-        self.K = self.aff_r**2
-        self.num_levels = num_levels
-        self.is_test = False
+#         super(TdwAffinityDataset, self).__init__(
+#             get_gt_segments=True,
+#             *args, **kwargs)
+#         self.aff_r = aff_r
+#         self.K = self.aff_r**2
+#         self.num_levels = num_levels
+#         self.is_test = False
 
-        self.raft = self._load_raft(raft_ckpt)
-        self.raft_args = copy.deepcopy(raft_args)
+#         self.raft = self._load_raft(raft_ckpt)
+#         self.raft_args = copy.deepcopy(raft_args)
 
-        self.full_supervision = full_supervision
-        self.flow_thresh = flow_thresh
+#         self.full_supervision = full_supervision
+#         self.flow_thresh = flow_thresh
 
-    def _load_raft(self, ckpt):
-        if ckpt is None:
-            return None
-        raft = train.load_model(
-            load_path=ckpt,
-            smalle=False,
-            cuda=True,
-            train=False)
-        return raft
+#     def _load_raft(self, ckpt):
+#         if ckpt is None:
+#             return None
+#         raft = train.load_model(
+#             load_path=ckpt,
+#             smalle=False,
+#             cuda=True,
+#             train=False)
+#         return raft
 
-    def _get_foreground(self, flow):
+#     def _get_foreground(self, flow):
 
-        flow_mag = flow.float().square().sum(-3, True).sqrt()
-        is_moving = (flow_mag > self.flow_thresh).float()
-        return is_moving
+#         flow_mag = flow.float().square().sum(-3, True).sqrt()
+#         is_moving = (flow_mag > self.flow_thresh).float()
+#         return is_moving
+
+#     def __getitem__(self, idx):
+
+#         img1, img2, flow, gt_segments = super().__getitem__(idx)
+#         print("segments", gt_segments.dtype, gt_segments.shape)
+#         if self.raft is not None:
+#             _, flow = self.raft(
+#                 img1[None].cuda(), img2[None].cuda(),
+#                 **self.raft_args)
+#             flow = flow.squeeze(0) # remove batch dim
+#             print("raft flow", flow.shape)
+
+#         foreground = self._get_foreground(flow if not self.full_supervision else gt_segments)
+#         print("foreground", foreground.shape)
+#         print("images", img1.shape, img2.shape)
+
+
+
+#         # return image, segments, affinities
+#         return (img1, foreground, None)
+
+class TdwAffinityDataset(Dataset):
+    def __init__(self,
+                 dataset_dir='/mnt/fs6/honglinc/dataset/tdw_playroom_small/',
+                 training=True,
+                 delta_time=1,
+                 frame_idx=5):
+        self.training = training
+        self.frame_idx = frame_idx
+        self.delta_time = delta_time
+
+        meta_path = os.path.join(dataset_dir, 'meta.json')
+        self.meta = json.loads(Path(meta_path).open().read())
+
+        if self.training:
+            self.file_list = glob.glob(os.path.join(dataset_dir, 'images', 'model_split_[0-9]*', '*[0-8]'))
+        else:
+            self.file_list = glob.glob(os.path.join(dataset_dir, 'images', 'model_split_[0-3]', '*9'))
+
+    def __len__(self):
+        return len(self.file_list)
 
     def __getitem__(self, idx):
+        file_name = self.file_list[idx]
+        image_1 = self.read_frame(file_name, frame_idx=self.frame_idx)
+        try:
+            image_2 = self.read_frame(file_name, frame_idx=self.frame_idx+self.delta_time)
+        except Exception as e:
+            image_2 = image_1.clone()  #  This will always give empty motion segments, so the loss will be zero
+            print('Encounter error:', e)
+        segment_colors = self.read_frame(file_name.replace('/images/', '/objects/'), frame_idx=self.frame_idx)
+        _, segment_map, gt_moving = self.process_segmentation_color(segment_colors, file_name)
 
-        img1, img2, flow, gt_segments = super().__getitem__(idx)
-        print("segments", gt_segments.dtype, gt_segments.shape)
-        if self.raft is not None:
-            _, flow = self.raft(
-                img1[None].cuda(), img2[None].cuda(),
-                **self.raft_args)
-            flow = flow.squeeze(0) # remove batch dim
-            print("raft flow", flow.shape)
+        return image_1, image_2, segment_map, gt_moving
 
-        foreground = self._get_foreground(flow if not self.full_supervision else gt_segments)
-        print("foreground", foreground.shape)
-        print("images", img1.shape, img2.shape)
+    @staticmethod
+    def read_frame(path, frame_idx):
+        image_path = os.path.join(path, format(frame_idx, '05d') + '.png')
+        return read_image(image_path)
 
+    @staticmethod
+    def _object_id_hash(objects, val=256, dtype=torch.long):
+        C = objects.shape[0]
+        objects = objects.to(dtype)
+        out = torch.zeros_like(objects[0:1, ...])
+        for c in range(C):
+            scale = val ** (C - 1 - c)
+            out += scale * objects[c:c + 1, ...]
+        return out
 
+    def process_segmentation_color(self, seg_color, file_name):
+        # convert segmentation color to integer segment id
+        raw_segment_map = self._object_id_hash(seg_color, val=256, dtype=torch.long)
+        raw_segment_map = raw_segment_map.squeeze(0)
 
-        # return image, segments, affinities
-        return (img1, foreground, None)
+        # remove zone id from the raw_segment_map
+        meta_key = 'playroom_large_v3_images/' + file_name.split('/images/')[-1] + '.hdf5'
+        zone_id = int(self.meta[meta_key]['zone'])
+        raw_segment_map[raw_segment_map == zone_id] = 0
+
+        # convert raw segment ids to a range in [0, n]
+        _, segment_map = torch.unique(raw_segment_map, return_inverse=True)
+        segment_map -= segment_map.min()
+
+        # gt_moving_mask
+        gt_moving = raw_segment_map == int(self.meta[meta_key]['moving'])
+
+        return raw_segment_map, segment_map, gt_moving
+
 
 if __name__ == '__main__':
 
     dataset = TdwAffinityDataset(
-        aff_r=5,
-        root='/data5/dbear/tdw_datasets/playroom_large_v3copy/',
-        filepattern="*[0-8]",
-        raft_ckpt=None
+        # raft_ckpt=None
     )
     print(len(dataset))
     inp = dataset[0]
+    for v in inp:
+        print(v.dtype, v.shape, v.amin(), v.amax())
 
-    args = train.get_args("")
-    net = train.RAFT(args)
+    # args = train.get_args("")
+    # net = train.RAFT(args)
